ad29dc2d82cae576a81013290ed728febca3680a
Farhaan Ali <alifarhaan655@gmail.com>
2026-06-12 14:36:35 +0530

    add db ad dependencies

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,1 +1,32 @@
+from fastapi import Depends, HTTPException , status
+from sqlalchemy.orm import Session
+from app.core.db import SessionLocal
+from fastapi.security import OAuth2PasswordBearer
+from app.core.security import decode_access_token
+from app.models.user import User
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl = "/auth/login")
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+def get_current_user(token:str = Depends(oauth2_scheme), db:Session = Depends(get_db)):
+    credentials_exception = HTTPException(
+        status_code = status.HTTP_401_UNAUTHORIZED,
+        detail = "Invalid authentication credentials",
+    )
+    payload = decode_access_token(token)
+    if payload is None:
+        raise credentials_exception
+    user_id = payload.get("sub")
+    if user_id is None:
+        raise credentials_exception
+    user = db.query(User).filter(User.id == int(user_id)).first()
+    if user is None:
+        raise credentials_exception
     
+    return user
