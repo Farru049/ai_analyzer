f54e6e3495eff0cdcf634b8f2cf02ecbc2dd0104
Farhaan Ali <alifarhaan655@gmail.com>
2026-06-25 23:35:19 +0530

    feat: add rate limiting to application routes

--- app/main.py
+++ app/main.py
@@ -4,9 +4,18 @@ from app.core.db import Base, engine
 from app.models import user,application
 from app.routes import application as application_routes
 from app.routes import ai as ai_routes
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.util import get_remote_address
+from slowapi.errors import RateLimitExceeded
+
+limiter = Limiter(key_func=get_remote_address)
 
 app = FastAPI(title = "AI Job Tracker")
 
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded,_rate_limit_exceeded_handler)
+
 app.include_router(auth.router)
+
 app.include_router(application_routes.router)
 app.include_router(ai_routes.router)
--- app/routes/application.py
+++ app/routes/application.py
@@ -1,29 +1,40 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Request
 from sqlalchemy.orm import Session
 from typing import List
 from app.models.user import User
 from app.dependencies import get_db, get_current_user
+from slowapi import Limiter
+from slowapi.util import get_remote_address
 from app.schemas.application import ApplicationResponse, ApplicationCreate, ApplicationUpdate
 from app.services.application_service import create_application, get_applications, get_application, update_application, delete_application
 
+limiter = Limiter(key_func=get_remote_address)
 router = APIRouter(prefix = "/applications", tags = ["applications"])
 
 @router.post("/", response_model = ApplicationResponse)
-def create(data:ApplicationCreate, db:Session = Depends(get_db), current_user:User = Depends(get_current_user)):
+@limiter.limit("5/minute")
+def create(request: Request,data:ApplicationCreate, db:Session = Depends(get_db), current_user:User = Depends(get_current_user)):
+    # ratelimiting
+    # Implement rate limiting logic here
+    
     return create_application(db, current_user.id, data)
 
 @router.get("/", response_model = List[ApplicationResponse])
-def get_all(db:Session = Depends(get_db), current_user:User = Depends(get_current_user)):
+@limiter.limit("30/minute")
+def get_all(request:Request,db:Session = Depends(get_db), current_user:User = Depends(get_current_user)):
     return get_applications(db, current_user.id)
 
 @router.get("/{application_id}",response_model = ApplicationResponse)
-def get_one(application_id:int, db:Session = Depends(get_db), current_user:User = Depends(get_current_user)):
+@limiter.limit("30/minute")
+def get_one(request:Request,application_id:int, db:Session = Depends(get_db), current_user:User = Depends(get_current_user)):
     return get_application(db, current_user.id, application_id)
 
 @router.put("/{application_id}", response_model = ApplicationResponse)
-def update(application_id:int, data:ApplicationUpdate, db:Session = Depends(get_db), current_user:User = Depends(get_current_user)):
+@limiter.limit("10/minute")
+def update(request:Request,application_id:int, data:ApplicationUpdate, db:Session = Depends(get_db), current_user:User = Depends(get_current_user)):
     return update_application(db, current_user.id, application_id, data)
 
 @router.delete("/{application_id}")
-def delete(application_id:int, db:Session = Depends(get_db), current_user:User = Depends(get_current_user)):
+@limiter.limit("10/minute")
+def delete(request:Request,application_id:int, db:Session = Depends(get_db), current_user:User = Depends(get_current_user)):
     return delete_application(db, current_user.id, application_id)
