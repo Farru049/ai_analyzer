114ff5d0a122ba037a2bd92731fc0c2eb860ad11
Farhaan Ali <alifarhaan655@gmail.com>
2026-06-27 11:41:11 +0530

    feat: rate limiting refactored application routes

--- app/routes/application.py
+++ app/routes/application.py
@@ -3,20 +3,15 @@ from sqlalchemy.orm import Session
 from typing import List
 from app.models.user import User
 from app.dependencies import get_db, get_current_user
-from slowapi import Limiter
-from slowapi.util import get_remote_address
+from app.core.rate_limiter import limiter
 from app.schemas.application import ApplicationResponse, ApplicationCreate, ApplicationUpdate
 from app.services.application_service import create_application, get_applications, get_application, update_application, delete_application
 
-limiter = Limiter(key_func=get_remote_address)
 router = APIRouter(prefix = "/applications", tags = ["applications"])
 
 @router.post("/", response_model = ApplicationResponse)
 @limiter.limit("5/minute")
 def create(request: Request,data:ApplicationCreate, db:Session = Depends(get_db), current_user:User = Depends(get_current_user)):
-    # ratelimiting
-    # Implement rate limiting logic here
-    
     return create_application(db, current_user.id, data)
 
 @router.get("/", response_model = List[ApplicationResponse])
